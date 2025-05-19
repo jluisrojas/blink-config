9ce5bb31281104931d81a101dda22293c01027a7
Jose Luis Rojas Aranda <joseluisrojasaranda@gmail.com>
2025-05-19 14:19:30 -0600

    colors

---
+++ vesper.js
@@ -0,0 +1,22 @@
+t.prefs_.set("color-palette-overrides", [
+  "#101010", // Ansi 0 (Black)
+  "#f5a1a1", // Ansi 1 (Red)
+  "#90b995", // Ansi 2 (Green)
+  "#e6b999", // Ansi 3 (Yellow)
+  "#ac9fcf", // Ansi 4 (Blue)
+  "#e2a2ca", // Ansi 5 (Magenta)
+  "#ea83a5", // Ansi 6 (Cyan)
+  "#a0a0a0", // Ansi 7 (White)
+  "#7e7e7e", // Ansi 8 (Bright Black)
+  "#ff8080", // Ansi 9 (Bright Red)
+  "#99ffe4", // Ansi 10 (Bright Green)
+  "#ffc299", // Ansi 11 (Bright Yellow)
+  "#b9aef1", // Ansi 12 (Bright Blue)
+  "#ecaad6", // Ansi 13 (Bright Magenta)
+  "#f5b190", // Ansi 14 (Bright Cyan)
+  "#ffffff", // Ansi 15 (Bright White)
+]);
+
+t.prefs_.set("foreground-color", "#ffffff");
+t.prefs_.set("background-color", "#101010");
+t.prefs_.set("cursor-color", "rgba(172,177,171,0.8)");
