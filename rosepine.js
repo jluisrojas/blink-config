eab120c87c46742c4bdd3d8e59c8569e2eacc21d
Jose Luis Rojas Aranda <joseluisrojasaranda@gmail.com>
2025-07-06 19:20:24 -0600

    rosepine

---
+++ rosepine.js
@@ -0,0 +1,22 @@
+t.prefs_.set("color-palette-overrides", [
+  "#26233a", // Ansi 0 (Black)
+  "#eb6f92", // Ansi 1 (Red)
+  "#31748f", // Ansi 2 (Green)
+  "#f6c177", // Ansi 3 (Yellow)
+  "#9ccfd8", // Ansi 4 (Blue)
+  "#c4a7e7", // Ansi 5 (Magenta)
+  "#ebbcba", // Ansi 6 (Cyan)
+  "#e0def4", // Ansi 7 (White)
+  "#908caa", // Ansi 8 (Bright Black)
+  "#eb6f92", // Ansi 9 (Bright Red)
+  "#31748f", // Ansi 10 (Bright Green)
+  "#f6c177", // Ansi 11 (Bright Yellow)
+  "#9ccfd8", // Ansi 12 (Bright Blue)
+  "#c4a7e7", // Ansi 13 (Bright Magenta)
+  "#ebbcba", // Ansi 14 (Bright Cyan)
+  "#e0def4", // Ansi 15 (Bright White)
+]);
+
+t.prefs_.set("foreground-color", "#e0def4");
+t.prefs_.set("background-color", "#191724");
+t.prefs_.set("cursor-color", "rgba(110,106,134,0.8)");
